a97bf7f6da8215e6fe983d3feeaff403c73f17d5
Samarth Ghadi <samarthghadi@gmail.com>
2023-01-05 19:53:37 -0500

    3rd save

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Samarth_Resume from '../../assets/Samarth_Resume.pdf'
+import cv from '../../assets/cv.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={Samarth_Resume} download className='btn'>Download Resume</a>
+        <a href={cv} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
     </div>
